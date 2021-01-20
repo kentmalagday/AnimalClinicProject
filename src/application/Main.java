8959de63927aa4465b43f996572d5ed67c9c24db
kentmalagday <77406902+kentmalagday@users.noreply.github.com>
2021-01-20 17:39:10 +0800

    Adding of Initial Files

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	 private static Stage stg;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+    	stg = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+    
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
