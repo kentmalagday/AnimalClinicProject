28bff20370d13feaa8cdcc7f412162423920fc87
kentmalagday <77406902+kentmalagday@users.noreply.github.com>
2021-02-03 17:59:30 +0800

    fix

---
+++ src/application/Appointments.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class Appointments {
+
+    @FXML
+    private TableColumn<?, ?> id;
+
+    @FXML
+    private TableColumn<?, ?> client;
+
+    @FXML
+    private TableColumn<?, ?> appointmentdate;
+
+    @FXML
+    private TableColumn<?, ?> time;
+
+}
+
