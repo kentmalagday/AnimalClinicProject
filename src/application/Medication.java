fbff4ee5eae377c6a50aa3fe896c7dc8d4dcbac4
kentmalagday <77406902+kentmalagday@users.noreply.github.com>
2021-02-11 11:43:04 +0800

    added medication.fxml

---
+++ src/application/Medication.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Medication implements Initializable{
+
+    @FXML
+    TableColumn<?, ?> description;
+
+    @FXML
+    TableColumn<?, ?> dosage;
+
+    @FXML
+    TableColumn<?, ?> status;
+
+    @FXML
+    TextArea textArea_description;
+
+    @FXML
+    TextField textField_dosage;
+
+    @FXML
+    Button btn_back;
+
+    @FXML
+    ComboBox<String> comboBox_status;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        ObservableList<String> list = FXCollections.observableArrayList("Taken", "Not Taken"); 
+		comboBox_status.setItems(list);
+    }
+    
+    public void btn_backClicked() throws IOException {
+        Main m = new Main();
+        m.changeScene("pethealthlogmain.fxml", "Pet Health Log", 1522, 839);
+    }
+}
