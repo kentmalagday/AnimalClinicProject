9bcc6c4d667ff5facb856d2b8f9da5206b0ba24d
kentmalagday <77406902+kentmalagday@users.noreply.github.com>
2021-02-11 20:18:34 +0800

    drug name

--- src/application/Medication.java
+++ src/application/Medication.java
@@ -19,7 +19,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.text.Text;
+
 
 public class Medication implements Initializable{
 	
@@ -27,6 +27,9 @@ public class Medication implements Initializable{
 	Label petname;
 	@FXML
 	TableView<PetMedication> medicationTable;
+
+	@FXML
+    TableColumn<?, ?> drugname;
 
     @FXML
     TableColumn<PetMedication, String> description;
